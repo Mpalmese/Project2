2ff0bc72aad9ab6cae87813cf08daaf498e665fd
Mpalmese <mark.palmese@gmail.com>
2019-05-21 16:06:01 -0400

    adding flask with line and map

--- app2.py
+++ app2.py
@@ -44,7 +44,7 @@ app = Flask(__name__)
 
 @app.route ("/")
 def home():
-    return render_template("index.html")
+    return render_template("index3.html")
 
 
 @app.route("/api/v1.0/lifedata")
@@ -100,14 +100,68 @@ def incomedata():
         url = 'http://api.worldbank.org/v2/country/'+ country_code + '?format=json'
         response = requests.get(url).json()
         income_dict = {}
-        income_dict['Country_Name'] = country_names[count]
         if response[1][0]['incomeLevel']['value'] != 'Aggregates':
+            income_dict['Country_Name'] = country_names[count]
             income_dict['Income_Level'] = response[1][0]['incomeLevel']['value']
+            income_dict['Lat'] = response[1][0]['latitude']
+            income_dict['Lng'] = response[1][0]['longitude']
             income_list.append(income_dict)
         count +=1
     
     return jsonify(income_list)
 
+@app.route("/api/v1.0/<country>")
+def country(country):
+    session = Session(engine)
+    liferesults = session.query(Life.Country_Name, Life.Country_Code, Life.Yr1999, Life.Yr2000,Life.Yr2001,Life.Yr2002,Life.Yr2003,Life.Yr2004,Life.Yr2005,Life.Yr2006,Life.Yr2007,Life.Yr2008,Life.Yr2009,Life.Yr2010,Life.Yr2011,Life.Yr2012,Life.Yr2013,Life.Yr2014,Life.Yr2015,Life.Yr2016,Life.Yr2017).filter(Life.Country_Code == country)
+
+    country_data = []
+    for name, code, y99, y00, y01, y02, y03, y04, y05, y06,y07,y08,y09,y10,y11,y12,y13,y14,y15,y16,y17 in liferesults:
+        if code == country:
+            country_dict = {}
+            country_dict['Country_Name'] = name
+            country_dict['Country_Code'] = code
+            country_dict['Yr1999'] = y99
+            country_dict['Yr2000'] = y00
+            country_dict['Yr2001'] = y01
+            country_dict['Yr2002'] = y02
+            country_dict['Yr2003'] = y03
+            country_dict['Yr2004'] = y04
+            country_dict['Yr2005'] = y05
+            country_dict['Yr2006'] = y06
+            country_dict['Yr2007'] = y07
+            country_dict['Yr2008'] = y08
+            country_dict['Yr2009'] = y09
+            country_dict['Yr2010'] = y10
+            country_dict['Yr2011'] = y11
+            country_dict['Yr2012'] = y12
+            country_dict['Yr2013'] = y13
+            country_dict['Yr2014'] = y14
+            country_dict['Yr2015'] = y15
+            country_dict['Yr2016'] = y16
+            country_dict['Yr2017'] = y17
+
+    country_codes = []
+    for row in life_db['Country_Code']:
+        country_codes.append(row)
+
+    for code in country_codes:
+        if code == country_dict['Country_Code']:
+            url = 'http://api.worldbank.org/v2/country/'+ code + '?format=json'
+            response = requests.get(url).json()
+
+            if response[1][0]['incomeLevel']['value'] != 'Aggregates':
+                country_dict['Income_Level'] = response[1][0]['incomeLevel']['value']
+                country_dict['Lat'] = response[1][0]['latitude']
+                country_dict['Lng'] = response[1][0]['longitude']
+
+            country_data.append(country_dict)
+
+    return jsonify(country_data)
+
+@app.route("/map")
+def map():
+    return render_template('index2.html')
 
 if __name__ == "__main__":
     app.run()
---
+++ static/js/linelogic.js
@@ -0,0 +1,246 @@
+function buildCharts(country) {
+    
+    d3.json(`/api/v1.0/${country}`).then(function(response) {
+    
+    var years = ['1999', '2000', '2001', '2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
+    var life_ex = [];
+    var country_name = [];
+    var incomelvl = [];
+    life_ex.push(response[0].Yr1999);
+    life_ex.push(response[0].Yr2000);
+    life_ex.push(response[0].Yr2001);
+    life_ex.push(response[0].Yr2002);
+    life_ex.push(response[0].Yr2003);
+    life_ex.push(response[0].Yr2004);
+    life_ex.push(response[0].Yr2005);
+    life_ex.push(response[0].Yr2006);
+    life_ex.push(response[0].Yr2007);
+    life_ex.push(response[0].Yr2008);
+    life_ex.push(response[0].Yr2009);
+    life_ex.push(response[0].Yr2010);
+    life_ex.push(response[0].Yr2011);
+    life_ex.push(response[0].Yr2012);
+    life_ex.push(response[0].Yr2013);
+    life_ex.push(response[0].Yr2014);
+    life_ex.push(response[0].Yr2015);
+    life_ex.push(response[0].Yr2016);
+    life_ex.push(response[0].Yr2017);
+    country_name.push(response[0].Country_Name);
+    incomelvl.push(response[0].Income_Level);
+    console.log(country_name[0]);
+
+    var trace1 = {
+        x: years,
+        y: life_ex,
+        type: "line",
+        name: country_name[0] + " - " + incomelvl[0],
+        line: {
+            color: 'blue'
+        }
+    };
+
+    
+        d3.json('/api/v1.0/HIC').then(function(response) {
+        
+        var highlife_ex = [];
+        
+        highlife_ex.push(response[0].Yr1999);
+        highlife_ex.push(response[0].Yr2000);
+        highlife_ex.push(response[0].Yr2001);
+        highlife_ex.push(response[0].Yr2002);
+        highlife_ex.push(response[0].Yr2003);
+        highlife_ex.push(response[0].Yr2004);
+        highlife_ex.push(response[0].Yr2005);
+        highlife_ex.push(response[0].Yr2006);
+        highlife_ex.push(response[0].Yr2007);
+        highlife_ex.push(response[0].Yr2008);
+        highlife_ex.push(response[0].Yr2009);
+        highlife_ex.push(response[0].Yr2010);
+        highlife_ex.push(response[0].Yr2011);
+        highlife_ex.push(response[0].Yr2012);
+        highlife_ex.push(response[0].Yr2013);
+        highlife_ex.push(response[0].Yr2014);
+        highlife_ex.push(response[0].Yr2015);
+        highlife_ex.push(response[0].Yr2016);
+        highlife_ex.push(response[0].Yr2017);
+        console.log(highlife_ex);
+        
+
+        var trace2 = {
+            x: years,
+            y: highlife_ex,
+            type: "line",
+            name: 'High Income',
+            line: {
+                color: 'green'
+            }
+        };
+        
+        
+        
+    
+    
+
+    
+        d3.json('/api/v1.0/LIC').then(function(response) {
+        
+        var lowlife_ex = [];
+        
+        lowlife_ex.push(response[0].Yr1999);
+        lowlife_ex.push(response[0].Yr2000);
+        lowlife_ex.push(response[0].Yr2001);
+        lowlife_ex.push(response[0].Yr2002);
+        lowlife_ex.push(response[0].Yr2003);
+        lowlife_ex.push(response[0].Yr2004);
+        lowlife_ex.push(response[0].Yr2005);
+        lowlife_ex.push(response[0].Yr2006);
+        lowlife_ex.push(response[0].Yr2007);
+        lowlife_ex.push(response[0].Yr2008);
+        lowlife_ex.push(response[0].Yr2009);
+        lowlife_ex.push(response[0].Yr2010);
+        lowlife_ex.push(response[0].Yr2011);
+        lowlife_ex.push(response[0].Yr2012);
+        lowlife_ex.push(response[0].Yr2013);
+        lowlife_ex.push(response[0].Yr2014);
+        lowlife_ex.push(response[0].Yr2015);
+        lowlife_ex.push(response[0].Yr2016);
+        lowlife_ex.push(response[0].Yr2017);
+        console.log(lowlife_ex);
+        
+
+        var trace5 = {
+            x: years,
+            y: lowlife_ex,
+            type: "line",
+            name: 'Low Income',
+            line: {
+                color: 'red'
+            }
+        };
+        
+        
+        
+    
+    
+        d3.json('/api/v1.0/UMC').then(function(response) {
+        
+        var umlife_ex = [];
+        
+        umlife_ex.push(response[0].Yr1999);
+        umlife_ex.push(response[0].Yr2000);
+        umlife_ex.push(response[0].Yr2001);
+        umlife_ex.push(response[0].Yr2002);
+        umlife_ex.push(response[0].Yr2003);
+        umlife_ex.push(response[0].Yr2004);
+        umlife_ex.push(response[0].Yr2005);
+        umlife_ex.push(response[0].Yr2006);
+        umlife_ex.push(response[0].Yr2007);
+        umlife_ex.push(response[0].Yr2008);
+        umlife_ex.push(response[0].Yr2009);
+        umlife_ex.push(response[0].Yr2010);
+        umlife_ex.push(response[0].Yr2011);
+        umlife_ex.push(response[0].Yr2012);
+        umlife_ex.push(response[0].Yr2013);
+        umlife_ex.push(response[0].Yr2014);
+        umlife_ex.push(response[0].Yr2015);
+        umlife_ex.push(response[0].Yr2016);
+        umlife_ex.push(response[0].Yr2017);
+        console.log(umlife_ex);
+        
+
+        var trace3 = {
+            x: years,
+            y: umlife_ex,
+            type: "line",
+            name: 'Upper middle income',
+            line: {
+                color: 'yellowgreen'
+            }
+        };
+        
+        
+        
+    
+    
+        d3.json('/api/v1.0/LMC').then(function(response) {
+        
+        var lmlife_ex = [];
+        
+        lmlife_ex.push(response[0].Yr1999);
+        lmlife_ex.push(response[0].Yr2000);
+        lmlife_ex.push(response[0].Yr2001);
+        lmlife_ex.push(response[0].Yr2002);
+        lmlife_ex.push(response[0].Yr2003);
+        lmlife_ex.push(response[0].Yr2004);
+        lmlife_ex.push(response[0].Yr2005);
+        lmlife_ex.push(response[0].Yr2006);
+        lmlife_ex.push(response[0].Yr2007);
+        lmlife_ex.push(response[0].Yr2008);
+        lmlife_ex.push(response[0].Yr2009);
+        lmlife_ex.push(response[0].Yr2010);
+        lmlife_ex.push(response[0].Yr2011);
+        lmlife_ex.push(response[0].Yr2012);
+        lmlife_ex.push(response[0].Yr2013);
+        lmlife_ex.push(response[0].Yr2014);
+        lmlife_ex.push(response[0].Yr2015);
+        lmlife_ex.push(response[0].Yr2016);
+        lmlife_ex.push(response[0].Yr2017);
+        console.log(lmlife_ex);
+        
+
+        var trace4 = {
+            x: years,
+            y: lmlife_ex,
+            type: "line",
+            name: 'Lower middle income',
+            line: {
+                color: 'chocolate'
+            }
+        };
+        
+        var data = [trace1, trace2, trace3, trace4, trace5];
+    
+        var layout = {
+            title: "Life Expectancy vs Income Level",
+            xaxis: {
+                title: "Year"
+            },
+            yaxis: {
+                title: "Life Expectancy",
+                range: [40,90]
+            }
+        };
+
+        Plotly.newPlot("line", data, layout, {responsive: true});
+        });
+    });
+    });
+    });
+});
+}
+
+
+
+function init() {
+    var selector = d3.select('#selDataset');
+
+    lifedata.forEach(function(data) {
+        selector
+            .append("option")
+            .text(data.Country_Name)
+            .property("value", data.Country_Code)
+    });
+
+    
+    // const firstchart = data[0].Country_Name;
+    // buildCharts(firstchart);
+    
+
+};
+
+function optionChanged(newdata) {
+    
+    buildCharts(newdata);
+}
+
+init();
